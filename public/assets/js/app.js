ea2228bb48b5da2ff6210defee4874c21ad40b69
Christopher de Beer <christopherdebeer@gmail.com>
2016-03-22 20:25:20 +0000

    update form

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,3 +1,22 @@
 $('.nav .expander').on( 'click', function(ev){
 	$(this).parent().toggleClass('expanded')
 })
+
+
+$('select').select2({width: '200px'});
+
+
+$('form').submit(function(ev){
+	if (!formCompleted()) {
+		alert("Oops! It looks like you havn't completed the RSVP form.")
+		ev.preventDefault();
+		return false;
+	}
+})
+
+
+function formCompleted() {
+	var name = $('input[name="name"').val();
+	console.log(name)
+	return ( name !== "");
+}
