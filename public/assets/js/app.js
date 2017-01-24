c9737c738648845452299cb29ce1f46376838561
Christopher de Beer <christopherdebeer@gmail.com>
2017-01-24 21:16:45 +0000

    Add transport feilds to rsvp form

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -8,7 +8,7 @@ $('.nav .expander').on( 'click', function(ev){
 
 $('form').submit(function(ev){
 	if (outstandingInputs().length > 0) {
-		alert("Oops! It looks like you havn't completed the RSVP form. Missing: " + outstandingInputs().join(', ') )
+		alert("Oops! It looks like you havn't completed the RSVP form. \nMissing: " + outstandingInputs().join(', ') )
 		ev.preventDefault();
 		return false;
 	}
@@ -16,6 +16,7 @@ $('form').submit(function(ev){
 
 
 $('form input[name="attending"]').on( 'change', function(ev){
+	console.log( $(this).val() )
 	var attending = $(this).val().slice(0,3) === "yes"
 	$('.ifAttending').toggle( attending );
 	$('.notAttending textarea').toggle( !attending );
@@ -28,17 +29,37 @@ $('form .requirements').on( 'change', function(ev){
 });
 
 
+$('form #number').on( 'change', function(ev){
+	$('.numSeats').text(ev.target.value);
+});
+
+
+
+
+
 function outstandingInputs() {
-	var name = $('input[name="name"]').val();
-	var attending = $('input[name="attending"]:checked').val().slice(0,3);
-	var email = $('input[name="email"]').val()
+
 	var outstanding = []
+	var name, attending, email, transport, number;
+
+	if (window.allowSubmit) {
+		attending = $('input[name="attending"]:checked').val().slice(0,3);
+	}
+	number = $('input[name="number"]').val()
+	name = $('input[name="name"]').val()
+	email = $('input[name="email"]').val()
+	transport = $('input[name="transport"]:checked').val()
+
+	console.log( number )
 
 	if (name === "") outstanding.push('Name(s)')
 	if (attending === "yes" && email === "") outstanding.push('Email')
-	if (!window.allowSubmit) outstanding.push('Attending')
+	if (!window.allowSubmit) outstanding.push('Attendance')
+	if (attending !== "no," && number === "") outstanding.push('Number Attending')
+	if (attending !== "no," && transport === "") outstanding.push('Transport')
 
-	console.log( outstanding, name, attending, email, window.allowSubmit )
+
+	console.log( outstanding, name, attending, email, transport, number, window.allowSubmit )
 
 	return outstanding;
 }
